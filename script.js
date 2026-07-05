5fbd0caa94a9686829439db7db0dca76bb95b0bd
Tanvir kabir <t6064773@gmail.com>
2026-07-05 23:48:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,241 @@
+ function Login(){
+    document.getElementById("loginpage").style.display="flex";
+     document.getElementById("header").style.display="none";
+     document.getElementById("content").style.display="none";
+     document.getElementById("menu1").style.display="none";
+      document.getElementById("padshow").style.display="none";
+      document.getElementById("mobileshow").style.display="none";
+      document.getElementById("menu2").style.display="none";
+      document.getElementById("footer").style.display="none";
+   
+       document.getElementById("order").style.display="none";
+        document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+   document.getElementById("service").style.display="none";
+    document.getElementById("menu3").style.display="none";
+    document.getElementById("menu4").style.display="none";
+    document.getElementById("menu5").style.display="none";
+    document.getElementById("menu6").style.display="none";
+    document.getElementById("menu7").style.display="none";
+    document.getElementById("menu8").style.display="none";
+    document.getElementById("menu9").style.display="none";
+     document.getElementById("laptopshow").style.display="none";
+      document.getElementById("watchshow").style.display="none";
+       document.getElementById("deskshow").style.display="none";
+        document.getElementById("camerashow").style.display="none";
+         document.getElementById("tvshow").style.display="none";
+          document.getElementById("speakshow").style.display="none";
+           document.getElementById("accessshow").style.display="none";
+
+  
+
+     }
+     function Registration(){
+    document.getElementById("registerpage").style.display="flex";
+     document.getElementById("header").style.display="none";
+     document.getElementById("content").style.display="none";
+     document.getElementById("menu1").style.display="none";
+      document.getElementById("padshow").style.display="none";
+      document.getElementById("mobileshow").style.display="none";
+      document.getElementById("menu2").style.display="none";
+      document.getElementById("order").style.display="none";
+         document.getElementById("footer").style.display="none";
+       document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+    document.getElementById("menu3").style.display="none";
+    document.getElementById("menu4").style.display="none";
+    document.getElementById("menu5").style.display="none";
+    document.getElementById("menu6").style.display="none";
+    document.getElementById("menu7").style.display="none";
+    document.getElementById("menu8").style.display="none";
+    document.getElementById("menu9").style.display="none";
+     document.getElementById("laptopshow").style.display="none";
+      document.getElementById("watchshow").style.display="none";
+       document.getElementById("deskshow").style.display="none";
+        document.getElementById("camerashow").style.display="none";
+         document.getElementById("tvshow").style.display="none";
+          document.getElementById("speakshow").style.display="none";
+           document.getElementById("accessshow").style.display="none";
+
+    
+
+     }
+     function home(){
+    document.getElementById("loginpage").style.display="none";
+    document.getElementById("registerpage").style.display="none";
+     document.getElementById("header").style.display="flex";
+     document.getElementById("content").style.display="grid";
+      document.getElementById("menu1").style.display="none";
+      document.getElementById("padshow").style.display="none";
+      document.getElementById("mobileshow").style.display="none";
+      document.getElementById("menu2").style.display="none";
+       document.getElementById("laptopshow").style.display="none";
+      document.getElementById("menu3").style.display="none";
+       document.getElementById("watchshow").style.display="none";
+      document.getElementById("menu4").style.display="none";
+       document.getElementById("camerashow").style.display="none";
+      document.getElementById("menu5").style.display="none";
+      document.getElementById("footer").style.display="flex";
+      document.getElementById("order").style.display="none";
+      document.getElementById("accessshow").style.display="none";
+      document.getElementById("menu7").style.display="none";
+       document.getElementById("tvshow").style.display="none";
+      document.getElementById("menu8").style.display="none";
+       document.getElementById("About").style.display="block";
+   document.getElementById("contuct").style.display="flex";
+   document.getElementById("speakshow").style.display="none";
+        document.getElementById("menu9").style.display="none";
+         document.getElementById("service").style.display="flex";
+          document.getElementById("deskshow").style.display="none";
+        document.getElementById("menu6").style.display="none";
+
+     }
+     function visit1(){
+
+   document.getElementById("mobileshow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu2").style.display="flex";
+    document.getElementById("content").style.display="none";
+   document.getElementById("footer").style.display="none";
+   document.getElementById("order").style.display="none";
+      document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+    
+     }
+     
+      function visit2(){
+
+   document.getElementById("padshow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu1").style.display="flex";
+    document.getElementById("content").style.display="none";
+    document.getElementById("footer").style.display="none";
+    document.getElementById("order").style.display="none";
+     document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+    
+     }
+     function visit3(){
+
+   document.getElementById("laptopshow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu3").style.display="flex";
+    document.getElementById("content").style.display="none";
+    document.getElementById("footer").style.display="none";
+    document.getElementById("order").style.display="none";
+     document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+    
+     }
+     function visit4(){
+
+   document.getElementById("watchshow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu4").style.display="flex";
+    document.getElementById("content").style.display="none";
+    document.getElementById("footer").style.display="none";
+    document.getElementById("order").style.display="none";
+     document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+    
+     }
+     function visit5(){
+
+   document.getElementById("camerashow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu5").style.display="flex";
+    document.getElementById("content").style.display="none";
+    document.getElementById("footer").style.display="none";
+    document.getElementById("order").style.display="none";
+     document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+    
+     }
+     function order(){
+        document.getElementById("order").style.display="flex";
+        document.getElementById("mobileshow").style.display="none";
+        document.getElementById("menu2").style.display="none";
+         document.getElementById("header").style.display="none";
+          document.getElementById("padshow").style.display="none";
+        document.getElementById("menu1").style.display="none";
+         document.getElementById("laptopshow").style.display="none";
+        document.getElementById("menu3").style.display="none";
+         document.getElementById("watchshow").style.display="none";
+        document.getElementById("menu4").style.display="none";
+         document.getElementById("camerashow").style.display="none";
+        document.getElementById("menu5").style.display="none";
+               document.getElementById("deskshow").style.display="none";
+        document.getElementById("menu6").style.display="none";
+          document.getElementById("accessshow").style.display="none";
+        document.getElementById("menu7").style.display="none";
+      document.getElementById("tvshow").style.display="none";
+        document.getElementById("menu8").style.display="none";
+         document.getElementById("speakshow").style.display="none";
+        document.getElementById("menu9").style.display="none";
+         document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+    
+    
+     }
+     function visit6(){
+
+   document.getElementById("deskshow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu6").style.display="flex";
+    document.getElementById("content").style.display="none";
+    document.getElementById("footer").style.display="none";
+    document.getElementById("order").style.display="none";
+     document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+     }
+     function visit7(){
+
+   document.getElementById("speakshow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu9").style.display="flex";
+    document.getElementById("content").style.display="none";
+    document.getElementById("footer").style.display="none";
+    document.getElementById("order").style.display="none";
+     document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+     }
+     function visit8(){
+
+   document.getElementById("accessshow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu7").style.display="flex";
+    document.getElementById("content").style.display="none";
+    document.getElementById("footer").style.display="none";
+    document.getElementById("order").style.display="none";
+     document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+     }
+     function visit9(){
+
+   document.getElementById("tvshow").style.display="grid";
+   document.getElementById("header").style.display="flex";
+    document.getElementById("menu8").style.display="flex";
+    document.getElementById("content").style.display="none";
+    document.getElementById("footer").style.display="none";
+    document.getElementById("order").style.display="none";
+     document.getElementById("About").style.display="none";
+   document.getElementById("contuct").style.display="none";
+    document.getElementById("service").style.display="none";
+     }
+     
+   
+
+
+
+   
+
